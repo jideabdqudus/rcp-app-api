9c69a94d5fbbc530c0a1c6b0a53a6ec379433d7f
Jide Abdul-Qudus <jideabdqudus@gmail.com>
2023-03-18 22:56:27 +0100

    wip: token api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,7 +2,8 @@
 Serializers for the user API View
 """
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (get_user_model, authenticate)
+from django.utils.translation import gettext as _ # Import translations
 
 # Import the serializer module from the rest_framework (serializers are basically a way to convert a json object to and from python, there are different base classes, we are using the serializers.ModelSerializer)
 from rest_framework import serializers
@@ -20,3 +21,29 @@ class UserSerializer(serializers.ModelSerializer):
   def create(self, validated_data):
     """Create and return a user with encrypted password"""
     return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+  """Serializer for the user auth token"""
+  email = serializers.EmailField() # Required field email
+  password = serializers.CharField( # Required field password
+    style={'input_type': 'password'}, # Hides password in browsable API while typing
+     # This ensures we dont trim whitespace at the end of the password
+    trim_whitespace = False
+  )
+
+  def validate(self, attrs):
+    """Validate and authenticate the user."""
+    email = attrs.get('email') # Retrieve email and password that user provides
+    password = attrs.get('password')
+    user = authenticate(
+      request=self.context.get('request'),
+      username=email, # we set username to the email address, per django uses username by defailt
+      password=password
+    )
+
+    if not user:
+      msg = _('Unable to authenticate with provided credentials.')
+      raise serializers.ValidationError(msg, code='authorization') # Standard way to raise error in serializers, the view would translate it to a HTPP 400 request
+
+    attrs['user'] = user
+    return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -3,10 +3,17 @@ Views from the user API
 """
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (UserSerializer, AuthTokenSerializer)
 
 
 class CreateUserView(generics.CreateAPIView):
   """Create a new user in the system"""
   serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+  """Create a new auth token for user."""
+  serializer_class = AuthTokenSerializer # Sets the serializer class to our own serializer that uses email as its username
+  renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES

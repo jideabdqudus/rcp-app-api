b6e4fa6a7ed544c56bbe759919ca2257b87bcdea
Jide Abdul-Qudus <jideabdqudus@gmail.com>
2023-03-19 12:08:10 +0100

    wip: create recipe model and app

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 Database models
 """
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
   AbstractBaseUser,
@@ -44,3 +45,23 @@ class User(AbstractBaseUser, PermissionsMixin):
   objects = UserManager()
 
   USERNAME_FIELD = 'email' # Sets the default field to email, and not username
+
+
+class Recipe(models.Model):
+  """Recipe object"""
+
+  # Belongs to this particular user, basically the model is attached to this user
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete= models.CASCADE, #If the related object gets deleted, it should also delete everything attached to their user = (models.CASCADE)
+  )
+  title = models.CharField(max_length=255)
+  description = models.TextField(blank=True) #TextField can hold more content with multiple lines
+  time_minutes = models.IntegerField()
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  link = models.CharField(max_length=255, blank=True)
+
+
+  def __str__(self):
+    return self.title
+
